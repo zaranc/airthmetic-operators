f18a51fdcccc472ea83d54211cfe72a86d3167ea
zaranc <130464977+zaranc@users.noreply.github.com>
2023-04-11 17:47:34 +0530

    Add files via upload

---
+++ arithmeticcalc.c
@@ -0,0 +1,25 @@
+# include<stdio.h>
+
+main()
+{
+	int a=20;
+	int b=5;
+    int c;
+    
+    
+    
+    c=a+b;
+    printf("Addition of %d and %d = %d \n" ,a,b,c);
+    c=a-b;
+    printf("Addition of %d and %d = %d \n" ,a,b,c);
+    c=a*b;
+    printf("Addition of %d and %d = %d \n" ,a,b,c);
+    c=a/b;
+    printf("Addition of %d and %d = %d \n" ,a,b,c);
+    c=a%b;
+    printf("Addition of %d and %d = %d \n" ,a,b,c);
+    
+    
+    
+    
+}
